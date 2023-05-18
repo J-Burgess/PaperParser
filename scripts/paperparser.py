bd8f5c318dfcaefa0e00fcfc7d0affff51b1ce7c
J-Burgess <jamesburgess96@hotmail.com>
2023-05-18 18:11:46 +0200

    FEAT: PDFS ARE NOW PARSED TO TEXT WITH PYPDF2

--- scripts/paperparser.py
+++ scripts/paperparser.py
@@ -1,7 +1,9 @@
 import requests
 import json
 import os
-from urllib.request import urlretrieve
+import glob
+import PyPDF2
+
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import CharacterTextSplitter
@@ -19,6 +21,46 @@ vectordb = Chroma(embedding_function=embedding, persist_directory=persist_direct
 #Now need to iteratively mine the pdfs into text and add embeddings into the vectorDB.
 #Need an ID for each paper and optional metadata. Citation/Title?
 
+def get_pdf_files(directory):
+    # Create the file path pattern to match PDF files
+    file_pattern = os.path.join(directory, "*.pdf")
+    # Use glob to get a list of PDF files
+    pdf_files = glob.glob(file_pattern)
+    return pdf_files
+
+def extract_text_from_pdf(pdf_path):
+    """
+    This function extracts text from the specified PDF file.
+    """
+    # Open the PDF file in read-binary mode.
+    with open(pdf_path, 'rb') as file:
+        # Create a PDF file reader object.
+        pdf = PyPDF2.PdfReader(file)
+        # Initialize an empty string to hold the extracted text.
+        text = ''
+        # Iterate through all the pages in the PDF file.
+        for page_num in range(len(pdf.pages)):
+            # Get a page object.
+            page = pdf.pages[page_num]
+            # Extract the text from the page.
+            page_text = page.extract_text()
+            # Add the extracted text to the overall text.
+            text += page_text + '\n'
+    # Return the extracted text.
+    return text
+
+
+# Specify the directory you want to search for PDF files
+pdf_directory_path = "/home/james/Desktop/Projects/paperparser/Papers"
+
+# Call the function to get the list of PDF files
+pdf_files_list = get_pdf_files(pdf_directory_path)
+
+# Print the list of PDF files
+for pdf_file in pdf_files_list:
+    #Parse into text and add to chroma db.
+    text = extract_text_from_pdf(pdf_file)
+    
 
 
 
