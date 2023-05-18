9a21d20b75667f351d613207626ee8bc54ceee26
J-Burgess <jamesburgess96@hotmail.com>
2023-05-18 19:32:31 +0200

    FEAT: PROMPT ENGINEERING TO ALWAYS OUTPUT DOI URLS

--- scripts/queryDB.py
+++ scripts/queryDB.py
@@ -2,7 +2,8 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.llms import OpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
-
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
 # Define your embedding model
 embedding = OpenAIEmbeddings()
 # Initialize the vector db
@@ -10,14 +11,23 @@ persist_directory = "/home/james/Desktop/Projects/paperparser/VectorDB"
 vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
 
 
+prompt_template = """
+You are a helpful assistant that has been given access to a vector memory of scientific papers and 
+tasked with returning summarized information and the citation based on the question given. 
+Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+{context}
+Question: {question}
+Always include the doi urls in your answer:"""
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+chain_type_kwargs = {"prompt": PROMPT}
+qa = RetrievalQA.from_chain_type(llm=OpenAI(temperature=0.1), chain_type="stuff", retriever=vectordb.as_retriever(),
+                                 chain_type_kwargs=chain_type_kwargs)
 
-
-
-qa = RetrievalQAWithSourcesChain.from_chain_type(llm=OpenAI(temperature=0.2), chain_type="stuff", retriever=vectordb.as_retriever())
-result = qa({"question": "What is a popular approach for decoding BCIs?"}, return_only_outputs=True)
-
-print("Answer: " + result["answer"].replace('\n', ' '))
-print("Source: " + result["sources"])
+query = "What is a popular approach for decoding BCIs? What are some pros and cons of each?"
+print(qa.run(query))
 
 
 
