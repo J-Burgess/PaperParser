ec2197582072727b34a515778ec529de00a87541
J-Burgess <jamesburgess96@hotmail.com>
2023-05-23 20:27:54 +0200

    FIX: SIMILARITY SEARCH NOW WORKS. HAD TO CLEAN UP CHARACTER INPUT.
    The pdf text splitting was retaining /n characters creating a super noisy embedding dataset. Removal of /n characters
    has facilitated successful query similarity vector search.

--- scripts/paperparser.py
+++ scripts/paperparser.py
@@ -18,70 +18,12 @@ from langchain.document_loaders import DirectoryLoader
 embedding = OpenAIEmbeddings()
 # Initialize the vector db
 persist_directory = "/home/james/Desktop/Projects/paperparser/VectorDB"
-vectordb = Chroma(embedding_function=embedding, persist_directory=persist_directory)
-
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 
 #Now need to iteratively mine the pdfs into text and add embeddings into the vectorDB.
 #Need an ID for each paper and optional metadata. Citation/Title?
 
-def get_pdf_files(directory):
-    # Create the file path pattern to match PDF files
-    file_pattern = os.path.join(directory, "*.pdf")
-    # Use glob to get a list of PDF files
-    pdf_files = glob.glob(file_pattern)
-    return pdf_files
-
-def get_txt_files(directory):
-    # Create the file path pattern to match PDF files
-    file_pattern = os.path.join(directory, "*.txt")
-    # Use glob to get a list of PDF files
-    txt_files = glob.glob(file_pattern)
-    return txt_files
-
-
-def extract_text_from_pdf(pdf_path):
-    """
-    This function extracts text from the specified PDF file.
-    """
-    # Open the PDF file in read-binary mode.
-    with open(pdf_path, 'rb') as file:
-        # Create a PDF file reader object.
-        pdf = PyPDF2.PdfReader(file)
-        # Initialize an empty string to hold the extracted text.
-        text = ''
-        # Iterate through all the pages in the PDF file.
-        for page_num in range(len(pdf.pages)):
-            # Get a page object.
-            page = pdf.pages[page_num]
-            # Extract the text from the page.
-            page_text = page.extract_text()
-            # Add the extracted text to the overall text.
-            text += page_text + '\n'
-    # Return the extracted text.
-    return text
-
-
 # Specify the directory you want to search for PDF files
 pdf_directory_path = "/home/james/Desktop/Projects/paperparser/Papers"
-
-# Call the function to get the list of PDF files
-pdf_files_list = get_pdf_files(pdf_directory_path)
-print(pdf_files_list)
-# Print the list of PDF files
-
-#Convert pdfs into text.
-for i in range(len(pdf_files_list)):
-    #Parse into text and add to chroma db.
-    text = extract_text_from_pdf(pdf_files_list[i])
-    #Save text to file
-    with open(f'{pdf_directory_path}/paper{i}.txt', 'w') as file:
-        # Write the text to the file
-        file.write(text)
-
-
-#Now need to split text files into chunks and embed into vectordb.
-txt_files = get_txt_files(pdf_directory_path)
 
 loader = DirectoryLoader(pdf_directory_path, glob="**/*.pdf")
 documents = loader.load()
@@ -90,7 +32,14 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 texts = text_splitter.split_documents(documents)
 print(texts)
 
+#For each of the texts, I need to clean the /n and special characters out of them before embedding.
+#Then I need to add them to the vectorDB.
+special_chars = ["\n", "\t", "\r", "\x0b", "\x0c"]
 
+print("Cleaning text...")
+for i in range(len(texts)):
+    texts[i].page_content = texts[i].page_content.replace("\n", "")
+    #print(texts[i].page_content)
 
 
 vectordb = Chroma.from_documents(texts, embedding, persist_directory=persist_directory)
--- scripts/queryDB.py
+++ scripts/queryDB.py
@@ -4,6 +4,7 @@ from langchain.llms import OpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 # Define your embedding model
 embedding = OpenAIEmbeddings()
 # Initialize the vector db
@@ -18,17 +19,27 @@ Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 {context}
 Question: {question}
-Always include the doi urls in your answer and the context you used to answer the question labelled with CONTEXT:"""
+Always include the doi urls from the context in your answer."""
 PROMPT = PromptTemplate(
     template=prompt_template, input_variables=["context", "question"]
 )
 chain_type_kwargs = {"prompt": PROMPT}
-qa = RetrievalQA.from_chain_type(llm=OpenAI(temperature=0), chain_type="stuff", retriever=vectordb.as_retriever(),
+qa = RetrievalQA.from_chain_type(llm=OpenAI(temperature=0.1), chain_type="stuff", retriever=vectordb.as_retriever(),
                                  chain_type_kwargs=chain_type_kwargs)
 
-query = "Can you summarize for me some pros and cons of SpecGAN to perform single molecule spectrum imaging?"
+query = "What is SpecGAN?"
+#Find relevant docs in vectorDB
+docs = vectordb.similarity_search(query)
+print(docs)
+
+
+
+
+
+
 print("QUERY: ", query)
-print(qa.run(query))
+#print(qa.run(query))
+
 
 
 
