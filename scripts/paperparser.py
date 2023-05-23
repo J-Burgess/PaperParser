6919d97aa25d2af2523a01fdc111a26e62ed1524
J-Burgess <jamesburgess96@hotmail.com>
2023-05-23 21:12:28 +0200

    FEAT: CONVERSATIONAL MEMORY INTRODUCED. SOURCE DOCUMENTS RETURNED TOO.

--- scripts/paperparser.py
+++ scripts/paperparser.py
@@ -1,14 +1,9 @@
-import requests
-import json
+
 import os
 import glob
-import PyPDF2
-
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
-from langchain.text_splitter import CharacterTextSplitter
 from langchain.llms import OpenAI
-from langchain.chains import RetrievalQA
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import DirectoryLoader
@@ -16,34 +11,28 @@ from langchain.document_loaders import DirectoryLoader
 
 # Define your embedding model
 embedding = OpenAIEmbeddings()
-# Initialize the vector db
+# Initialize the vector db persistence location
 persist_directory = "/home/james/Desktop/Projects/paperparser/VectorDB"
-
-#Now need to iteratively mine the pdfs into text and add embeddings into the vectorDB.
-#Need an ID for each paper and optional metadata. Citation/Title?
-
 # Specify the directory you want to search for PDF files
 pdf_directory_path = "/home/james/Desktop/Projects/paperparser/Papers"
 
+
+print("Loading documents...")
 loader = DirectoryLoader(pdf_directory_path, glob="**/*.pdf")
 documents = loader.load()
-print(len(documents))
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 texts = text_splitter.split_documents(documents)
-print(texts)
 
-#For each of the texts, I need to clean the /n and special characters out of them before embedding.
-#Then I need to add them to the vectorDB.
-special_chars = ["\n", "\t", "\r", "\x0b", "\x0c"]
 
+#For each of the texts, I need to clean the /n (and special characters potentially? TODO) out of them before embedding.
 print("Cleaning text...")
 for i in range(len(texts)):
     texts[i].page_content = texts[i].page_content.replace("\n", "")
-    #print(texts[i].page_content)
 
-
+print("Embedding text...")
 vectordb = Chroma.from_documents(texts, embedding, persist_directory=persist_directory)
 vectordb.persist()
+#Clear from memory
 vectordb = None
 
 
--- scripts/queryDB.py
+++ scripts/queryDB.py
@@ -1,44 +1,37 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.llms import OpenAI
-from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.prompts import PromptTemplate
-from langchain.chains import RetrievalQA
-from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
 # Define your embedding model
 embedding = OpenAIEmbeddings()
 # Initialize the vector db
 persist_directory = "/home/james/Desktop/Projects/paperparser/VectorDB"
 vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
 
+#initialize the memory
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, output_key='answer')
+#initialize the qa chain
+qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0), vectordb.as_retriever(), memory=memory,
+                                           return_source_documents=True)
 
-prompt_template = """
-You are a helpful assistant that has been given access to a vector memory of scientific papers and 
-tasked with returning summarized information and the citation based on the question given. 
-Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
-{context}
-Question: {question}
-Always include the doi urls from the context in your answer."""
-PROMPT = PromptTemplate(
-    template=prompt_template, input_variables=["context", "question"]
-)
-chain_type_kwargs = {"prompt": PROMPT}
-qa = RetrievalQA.from_chain_type(llm=OpenAI(temperature=0.1), chain_type="stuff", retriever=vectordb.as_retriever(),
-                                 chain_type_kwargs=chain_type_kwargs)
 
+#Now I need to query the database with a question and a followup question.
 query = "What is SpecGAN?"
-#Find relevant docs in vectorDB
-docs = vectordb.similarity_search(query)
-print(docs)
+
+result = qa({"question": query})
+print("QUERY: ", query)
+print("ANSWER: ", result["answer"])
+print("SOURCE: ", result["source_documents"][0])
 
 
-
-
-
+query = "What is the difference between SpecGAN and GAN?"
+result = qa({"question": query})
 
 print("QUERY: ", query)
-#print(qa.run(query))
+print("ANSWER: ", result["answer"])
+print("SOURCE: ", result["source_documents"][0])
 
 
 
